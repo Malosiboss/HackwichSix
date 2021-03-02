aca612ae1e4f805fc12ab91e2e9170ca043cc8de
NoahNua <NoahNua@hawaii.edu>
2021-03-02 10:54:49 -1000

    Problem set 1 complete

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -9,28 +9,43 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var myFriendsArray = ["Kaleb", "Autin", "ABBZ"]
+//   var myFriendsArray = ["Kaleb", "Autin", "ABBZ"]
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return myFriendsArray.count
+
+//   }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+//        let text = myFriendsArray[indexPath.row]
+//        cell.textLabel?.text = text
+//        return cell
+//    }
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//         Do any additional setup after loading the view.
+//    }
+    
+    var countriesCity = ["Japan", "Ireland", "New Zeland"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myFriendsArray.count
-
-   }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
-        let text = myFriendsArray[indexPath.row]
-        cell.textLabel?.text = text
-        return cell
+        return countriesCity.count
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    
    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+              let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+              let text = countriesCity[indexPath.row]
+              cell.textLabel?.text = text
+              return cell
+    }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        
  
 
 }
 
+}
