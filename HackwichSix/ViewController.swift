fba7e15558affd60a5ceefc8b5a7bb724a4a2d47
NoahNua <NoahNua@hawaii.edu>
2021-03-04 10:58:29 -1000

    Set subtitle text in Tableview

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -9,43 +9,51 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-//   var myFriendsArray = ["Kaleb", "Autin", "ABBZ"]
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return myFriendsArray.count
-
-//   }
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
-//        let text = myFriendsArray[indexPath.row]
-//        cell.textLabel?.text = text
-//        return cell
-//    }
-
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//         Do any additional setup after loading the view.
-//    }
-    
-    var countriesCity = ["Japan", "Ireland", "New Zeland"]
+var myFriendsArray = ["Kaleb", "Autin", "ABBZ"]
+    var friendsHomeArray = ["Honolulu", "Ewa beach", "Japan"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countriesCity.count
-    }
-   
+        return myFriendsArray.count
+
+   }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-              let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
-              let text = countriesCity[indexPath.row]
-              cell.textLabel?.text = text
-              return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriendsArray[indexPath.row]
+        cell.textLabel?.text = text
+        cell.detailTextLabel?.text = friendsHomeArray[indexPath.row]
+
+        return cell
     }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//         Do any additional setup after loading the view.
+ }
+  
+    
+    
+    
+    
+    
+//    var countriesCity = ["Japan", "Ireland", "New Zeland"]
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return countriesCity.count
+//    }
+   
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//              let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+//              let text = countriesCity[indexPath.row]
+//              cell.textLabel?.text = text
+//              return cell
+//    }
+//
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
         
  
 
 }
 
-}
+
