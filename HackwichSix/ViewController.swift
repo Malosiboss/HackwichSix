a754c46082e61b4e65ce5e4e765a7501ae65ab9d
NoahNua <NoahNua@hawaii.edu>
2021-04-22 09:47:47 -1000

    IT WORKS

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
         let dict = NSDictionary(contentsOfFile: path!)
         
-        restaurantImageData = dict!.object(forKey: "resturauntImages") as! [String]
+        restaurantImageData = dict!.object(forKey: "restaurauntImages") as! [String]
         
         
  }
@@ -44,7 +44,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        if segue.identifier == "mySegue"{
+      
+        if segue.identifier == "mySegue"
+        {
             let s1 = segue .destination as! detailViewController
             let imageIndex = tableView.indexPathForSelectedRow?.row
             s1.imagePass = restaurantImageData[imageIndex!]
